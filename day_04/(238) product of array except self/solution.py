eafbf92b9537c218143eb69bbb8adeffe8ebcd52
Marco Ponce <marcpon8@gmail.com>
2025-08-07 16:11:52 -0400

    day 4 and 5

---
+++ day_04/(238) product of array except self/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        res = [1] * n
+
+        prev = 1
+        for i in range(n):
+            res[i] = prev
+            prev *= nums[i]
+
+        post = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= post
+            post *= nums[i]
+
+        return res
