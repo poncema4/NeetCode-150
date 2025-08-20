38229d04f6bd1ae0b8d0534a847313089fcf355e
Marco Ponce <marcpon8@gmail.com>
2025-08-20 18:15:35 -0400

    day 14 done

---
+++ day_14/(153) find min in rotated sorted array/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] < nums[r]:
+                r = m
+            else:
+                l = m + 1
+
+        return nums[l]
