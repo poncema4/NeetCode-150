06264e104ec26eda54380782123cbcaa91666380
poncema4 <marcpon8@gmail.com>
2025-10-06 14:43:49 -0400

    updated day 25 with some tests

--- day_25/(206) reverse linked list/solution.py
+++ day_25/(206) reverse linked list/solution.py
@@ -1,8 +1,16 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+            result = []
+            curr = self
+            while curr:
+                result.append(str(curr.val))
+                curr = curr.next
+            return " -> ".join(result)
 
 class Solution(object):
     def reverseList(self, head):
@@ -19,3 +27,12 @@ class Solution(object):
             curr = temp
 
         return prev
+    
+l = ListNode(1)
+l.next = ListNode(2)
+l.next.next = ListNode(3)
+l.next.next.next = ListNode(4)
+l.next.next.next.next = ListNode(5)
+
+c = Solution()
+print(c.reverseList(l))
--- day_25/(21) merge two sorted lists/solution.py
+++ day_25/(21) merge two sorted lists/solution.py
@@ -4,6 +4,14 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+    def __str__(self):
+        result = []
+        curr = self
+        while curr:
+            result.append(str(curr.val))
+            curr = curr.next
+        return " -> ".join(result)
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -17,7 +25,7 @@ class Solution(object):
         while list1 and list2:
             if list1.val < list2.val:
                 tail.next = list1
-                list1 = list1.nextclea
+                list1 = list1.next
             else:
                 tail.next = list2
                 list2 = list2.next
@@ -26,3 +34,17 @@ class Solution(object):
         tail.next = list1 or list2
 
         return dummy.next
+    
+def create_linked_list(values):
+    dummy = ListNode()
+    curr = dummy
+    for v in values:
+        curr.next = ListNode(v)
+        curr = curr.next
+    return dummy.next
+
+l1 = create_linked_list([1, 2, 4, 7, 11])
+l2 = create_linked_list([1, 3, 4, 9, 12])
+
+c = Solution()
+print(c.mergeTwoLists(l1, l2))
