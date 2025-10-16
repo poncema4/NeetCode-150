ecfa9fae0cd57800103f86c56a60168a07ebd56f
poncema4 <marcpon8@gmail.com>
2025-10-16 13:38:42 -0400

    day 26 done

---
+++ day_26/(141) linked-list cycle/solution.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        result = []
+        curr = self
+        while curr:
+            result.append(str(curr.val))
+            curr = curr.next
+        return " -> ".join(result)
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+    
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(3)
+l1.next.next.next = ListNode(4)
+l1.next.next.next.next = ListNode(5)
+
+l2 = ListNode(1)
+l2.next = ListNode(2)
+l2.next.next = ListNode(3)
+l2.next.next.next = ListNode(4)
+l2.next.next.next.next = ListNode(5)
+l2.next.next.next.next.next = l2.next.next
+
+c = Solution()
+
+print("Test 1 (No cycle):", c.hasCycle(l1))
+print("Test 2 (With cycle):", c.hasCycle(l2))
