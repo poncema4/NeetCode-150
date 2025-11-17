e041822a9f8fe94b8a516f8906ff0a32ef8d7802
poncema4 <marcpon8@gmail.com>
2025-11-17 13:19:44 -0500

    day 33 done

---
+++ day_33/(110) balanced binary tree/solution.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def dfs(root):
+            if not root: 
+                return [True, 0]
+            
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+
+def printTree(root):
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        result.append(node.val)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return result
+
+c = Solution()
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(c.isBalanced(root))
+
+root2 = TreeNode(1)
+root2.left = TreeNode(2)
+root2.right = TreeNode(2)
+root2.left.left = TreeNode(3)
+root2.left.right = TreeNode(3)
+root2.left.left.left = TreeNode(4)
+root2.left.left.right = TreeNode(4)
+print(c.isBalanced(root2))
