8edfe863e7786b2814242cbf69805e18f8b269ef
Marco Ponce <marcpon8@gmail.com>
2025-12-23 12:19:56 -0500

    day 37 done

---
+++ day_37/(1448) count good nodes/solution.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def goodNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
+    
+def buildTree(nodes):
+    if not nodes:
+        return None
+    root = TreeNode(nodes[0])
+    queue = deque([root])
+    i = 1
+    while queue and i < len(nodes):
+        current = queue.popleft()
+        if nodes[i] is not None:
+            current.left = TreeNode(nodes[i])
+            queue.append(current.left)
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            current.right = TreeNode(nodes[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+c = Solution()
+tree = buildTree([3,1,4,3,None,1,5])
+print(c.goodNodes(tree))
