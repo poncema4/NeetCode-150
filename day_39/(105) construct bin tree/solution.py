e5f2f5f97a1918edec075e5556a0afa93b87248c
Marco Ponce <marcpon8@gmail.com>
2025-12-27 19:46:57 -0500

    done with easy and med trees

---
+++ day_39/(105) construct bin tree/solution.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
+
+def serialize(root):
+    if not root:
+        return []
+
+    result = []
+    q = deque([root])
+
+    while q:
+        node = q.popleft()
+        if node:
+            result.append(node.val)
+            q.append(node.left)
+            q.append(node.right)
+        else:
+            result.append(None)
+
+    while result and result[-1] is None:
+        result.pop()
+
+    return result
+
+c = Solution()
+tree = c.buildTree(preorder=[3,9,20,15,7], inorder=[9,3,15,20,7])
+print(serialize(tree))
